e5e8bf8eb4538fea89b1115877be49bd0455391c
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-04-17 16:57:46 +0200

    build: tested particles plugin

--- plugins/particles/src/index.tsx
+++ plugins/particles/src/index.tsx
@@ -3,19 +3,62 @@ import { Application, JSX, ParameterType } from "typedoc";
 export function load(app: Application): void {
   app.options.addDeclaration({
     name: "particlesOptions",
-    type: ParameterType.Mixed,
-    help: "Google Ads Pub ID",
-    defaultValue: {},
+    type: ParameterType.Object,
+    help: "Particles Options",
+    defaultValue: {
+      fullScreen: {
+        zIndex: -1,
+      },
+      particles: {
+        color: {
+          value: "#000",
+        },
+        links: {
+          color: "#000",
+          enable: true,
+          opacity: 0.5,
+          distance: 200,
+        },
+        move: {
+          enable: true,
+          speed: 0.2,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 100,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        size: {
+          value: 1,
+        },
+      },
+    },
+  });
+
+  app.options.addDeclaration({
+    name: "additionalPlugins",
+    type: ParameterType.Array,
+    help: "Additional Plugins",
+    defaultValue: [],
   });
 
   app.renderer.hooks.on("body.end", (ctx) => {
+    const particlesOptions = ctx.options.getValue("particlesOptions"),
+      additionalPlugins = ctx.options.getValue("additionalPlugins") as string[],
+      html = `tsParticles.load("tsparticles", ${JSON.stringify(particlesOptions)});`;
+
     return (
       <>
         <div id="tsparticles"></div>
-        <script src="https://cdn.jsdelivr.net/npm/tsparticles@2.9.3/tsparticles.bundle.min.js"></script>
-        <script type="text/javascript">
-          {`tsParticles.load("tsparticles", ${JSON.stringify(ctx.options.getValue("particlesOptions"))});`}
-        </script>
+        <script src="https://cdn.jsdelivr.net/npm/tsparticles@2"></script>
+        {additionalPlugins.map((plugin) => (
+          <script src={`https://cdn.jsdelivr.net/npm/${plugin}@2`}></script>
+        ))}
+        <script type="text/javascript">{<JSX.Raw html={html}></JSX.Raw>}</script>
       </>
     );
   });
