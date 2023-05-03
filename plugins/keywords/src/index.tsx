2bab0de99eda5f1ed128d3731f590773ae242041
Mirco Veltri <github@mircoveltri.me>
2023-05-03 10:52:39 +0200

    fix: meta name and content

--- plugins/keywords/src/index.tsx
+++ plugins/keywords/src/index.tsx
@@ -10,6 +10,6 @@ export function load(app: Application): void {
   app.renderer.hooks.on("head.begin", (ctx) => {
     const keywords = ctx.options.getValue("keywords") as string[];
 
-    return <meta name="keyword" content={keywords.join(" ")} />;
+    return <meta name="keywords" content={keywords.join(", ")} />;
   });
 }
